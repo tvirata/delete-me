c2b19d89fc96e04709339400d77cb7063b027006
tvirata <45518488+tvirata@users.noreply.github.com>
2023-09-19 20:09:19 -0700

    9.19.23
    scss, parallax, javascript added

---
+++ test.js
@@ -0,0 +1,13 @@
+console.log("This is javascript");
+// single comment
+/*
+    multiple comments
+*/
+
+const a = 1; // no datatypes
+let b = "word"; // if something can be a variable, use let
+console.log(b);
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
